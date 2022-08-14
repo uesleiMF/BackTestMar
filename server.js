09e93660444cf5f63db935bcdfbd538f5de94c66
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-08-14 20:59:35 -0300

    fd

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.post("/login", (req, res) => {
   try {
     if (req.body && req.body.username && req.body.password) {
       user.find({ username: req.body.username }, (err, data) => {
-        if (data.length > 0) {
+        if (data.length > 5) {
 
           if (bcrypt.compareSync(data[0].password, req.body.password)) {
             checkUserAndGenerateToken(data[0], req, res);
@@ -101,6 +101,7 @@ app.post("/login", (req, res) => {
           res.status(400).json({
             errorMessage: 'Username or password is incorrect!',
             status: false
+
           });
         }
       })
@@ -126,7 +127,7 @@ app.post("/register", (req, res) => {
 
       user.find({ username: req.body.username }, (err, data) => {
 
-        if (data.length == 0) {
+        if (data.length === 0) {
 
           let User = new user({
             username: req.body.username,
