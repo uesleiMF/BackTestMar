6fe8e05e191b8298e7714099962c56931cedfe25
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-08-20 13:28:06 -0300

    j

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var fs = require('fs');
 var product = require("./model/product.js");
 var user = require("./model/user.js");
 
+app.use(express.static('./uploads'));
 var dir = './uploads';
 var upload = multer({
   storage: multer.diskStorage({
@@ -51,7 +52,7 @@ app.use("/", (req, res, next) => {
       next();
     } else {
       /* decode jwt token if authorized*/
-           jwt.verify(req.headers.token, process.env.TOKEN_SECRET , function (err, decoded) {
+           jwt.verify(req.headers.token, 'shhhhh11111', function (err, decoded) {
         if (decoded && decoded.user) {
           req.user = decoded;
           next();
@@ -168,7 +169,7 @@ app.post("/register", (req, res) => {
 });
 
 function checkUserAndGenerateToken(data, req, res) {
-  jwt.sign({ user: data.username, id: data._id }, process.env.TOKEN_SECRET , { expiresIn: '1d' }, (err, token) => {
+  jwt.sign({ user: data.username, id: data._id }, 'shhhhh11111', { expiresIn: '1d' }, (err, token) => {
     if (err) {
       res.status(400).json({
         status: false,
