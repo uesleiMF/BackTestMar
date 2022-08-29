95bfc72b88ba5db74cf9d3823272fb493fccab13
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2022-08-29 17:46:00 -0300

    Update server.js
    Bol

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var upload = multer({
   }
 });
 app.use(cors());
-app.use(express.static(path.resolve(__dirname,'uploads'));
+app.use(express.static('uploads'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 extended: true
