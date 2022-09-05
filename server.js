fa29b8c58f67f404e47e07eb83dc840451f2a18a
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-09-04 21:21:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -44,17 +44,8 @@ app.use(cors());
 app.use(express.static('uploads'));
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-<<<<<<< HEAD
-  extended: false
-=======
-extended: true
->>>>>>> b6ef3290862b3b33c36590c4e11de83aca5f7388
+extended: false
 }));
-
-
-
-
-
 
 
 app.use("/", (req, res, next) => {
@@ -64,11 +55,7 @@ app.use("/", (req, res, next) => {
     } else {
       /* decode jwt token if authorized*/
 
-<<<<<<< HEAD
            jwt.verify(req.headers.token,secret, function (err, decoded) {
-=======
-           jwt.verify(req.headers.token, 'shhhh1111', function (err, decoded) {
->>>>>>> b6ef3290862b3b33c36590c4e11de83aca5f7388
         if (decoded && decoded.user) {
           req.user = decoded;
           next();
@@ -185,12 +172,9 @@ app.post("/register", (req, res) => {
 });
 
 function checkUserAndGenerateToken(data, req, res) {
-<<<<<<< HEAD
+  
   jwt.sign({ user: data.username, id: data._id },secret, { expiresIn: '1d' }, (err, token) => {
-=======
-  jwt.sign({ user: data.username, id: data._id }, 'shhhh1111', { expiresIn: '3d' }, (err, token) => {
->>>>>>> b6ef3290862b3b33c36590c4e11de83aca5f7388
-    if (err) {
+     if (err) {
       res.status(400).json({
         status: false,
         errorMessage: err,
