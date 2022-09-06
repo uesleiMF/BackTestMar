f4b6d59c020287bd0b2fb36f7259267519797ad6
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-09-06 15:13:41 -0300

    agora

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ var upload = multer({
 });
 
 app.use(cors());
-app.use(express.static('uploads'));
+app.use(express.static(path.join(__dirname,"uploads")));
+//app.use(express.static('uploads'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
